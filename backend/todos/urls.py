4097adb28faa9d15dc6002dfec5efa6fab9ed549
Rahmad3067 <Aboozar919@gmail.com>
2022-01-19 12:57:11 +0100

    first reac front and django REST framework as bac

---
+++ backend/todos/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import ListTodo, DetailTodo
+
+
+
+
+
+
+
+urlpatterns = [
+    path('<int:pk>/', DetailTodo.as_view()),
+    path('', ListTodo.as_view()),
+]
